aa470359d4e12da643962a3857f485227f511f21
t_adachi <int_haimaki.mt32@ezweb.ne.jp>
2016-05-30 00:35:18 +0900

    新着記事　詳細表示画面、編集画面、バリデートチェック作成

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -2,3 +2,8 @@ function article_page() {
   location.href = Routes.articles_path();
   return false;
 }
+
+function article_show_page(id) {
+  location.href = Routes.article_path(id);
+  return false;
+}
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,55 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = Article.all
+    @articles = Article.where(deleted: false).order("updated_at DESC")
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
-    redirect_to action: :index
+    @article = Article.new(article_params)
+    today    = Date.today
+
+    @article.user_id         = session[:userid]
+    @article.registered_date = today.strftime("%Y%m%d")
+    @article.updated_date    = today.strftime("%Y%m%d")
+
+    if @article.save
+      flash[:notice] = t("success.process")
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  def update
+    @article              = Article.find(params[:id])
+    @article.attributes   = article_params
+    today                 = Date.today
+    @article.user_id      = session[:userid]
+    @article.updated_date = today.strftime("%Y%m%d")
+
+    if @article.save
+      flash[:notice] = t("success.process")
+      redirect_to action: :index
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
