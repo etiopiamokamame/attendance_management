e5d1cb7a98fe0f9e8b46df3cbc7ead1d12565d18
adachi <int_haimaki.mt32@ezweb.ne>
2016-12-14 22:50:23 +0900

    システム設定機能一旦完了

--- app/models/system_config.rb
+++ app/models/system_config.rb
@@ -57,59 +57,38 @@ class SystemConfig < ApplicationRecord
   end
 
   def base_working_start_time_text=(value)
-    binding.pry
-    # self.base_working_start_time = Date.strptime(value, I18n.t(:date_format)).strftime(I18n.t(:default_date_format))
+    time = Time.strptime(value, I18n.t(:default_time_format, separate: CONSTANTS::TIME_SEPARATE_TAG))
+    self.base_working_start_time = time.strftime(I18n.t(:default_time_format, separate: nil))
   rescue
     self.base_working_start_time = nil
   end
 
   def base_working_end_time_text=(value)
-    binding.pry
-    # self.base_working_end_time = Date.strptime(value, I18n.t(:date_format)).strftime(I18n.t(:default_date_format))
+    time = Time.strptime(value, I18n.t(:default_time_format, separate: CONSTANTS::TIME_SEPARATE_TAG))
+    self.base_working_end_time = time.strftime(I18n.t(:default_time_format, separate: nil))
   rescue
     self.base_working_end_time = nil
   end
 
   def rest_start_time_text=(value)
-    binding.pry
-    # self.rest_start_time = Date.strptime(value, I18n.t(:date_format)).strftime(I18n.t(:default_date_format))
+    time = Time.strptime(value, I18n.t(:default_time_format, separate: CONSTANTS::TIME_SEPARATE_TAG))
+    self.rest_start_time = time.strftime(I18n.t(:default_time_format, separate: nil))
   rescue
     self.rest_start_time = nil
   end
 
   def rest_end_time_text=(value)
-    binding.pry
-    # self.rest_end_time = Date.strptime(value, I18n.t(:date_format)).strftime(I18n.t(:default_date_format))
+    time = Time.strptime(value, I18n.t(:default_time_format, separate: CONSTANTS::TIME_SEPARATE_TAG))
+    self.rest_end_time = time.strftime(I18n.t(:default_time_format, separate: nil))
   rescue
     self.rest_end_time = nil
   end
 
-  def base_overtime_rest_start_time_text=(value)
-    binding.pry
-    # self.base_overtime_rest_start_time = Date.strptime(value, I18n.t(:date_format)).strftime(I18n.t(:default_date_format))
-  rescue
-    self.base_overtime_rest_start_time = nil
-  end
-
-  def base_overtime_rest_end_time_text=(value)
-    binding.pry
-    # self.base_overtime_rest_end_time = Date.strptime(value, I18n.t(:date_format)).strftime(I18n.t(:default_date_format))
-  rescue
-    self.base_overtime_rest_end_time = nil
-  end
-
   def late_night_time_text=(value)
-    binding.pry
-    # self.late_night_time = Date.strptime(value, I18n.t(:date_format)).strftime(I18n.t(:default_date_format))
+    time = Time.strptime(value, I18n.t(:default_time_format, separate: CONSTANTS::TIME_SEPARATE_TAG))
+    self.late_night_time = time.strftime(I18n.t(:default_time_format, separate: nil))
   rescue
     self.late_night_time = nil
-  end
-
-  def time_off_hours_prospect_text=(value)
-    binding.pry
-    # self.time_off_hours_prospect = Date.strptime(value, I18n.t(:date_format)).strftime(I18n.t(:default_date_format))
-  rescue
-    self.time_off_hours_prospect = nil
   end
 
   def weeks
